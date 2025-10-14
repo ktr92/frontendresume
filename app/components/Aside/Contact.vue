b5a6189cf913a489e65fc7303f70686f497f8ad3
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2025-10-14 22:44:57 +0300

    contacts

---
+++ app/components/Aside/Contact.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="border-none">
+    <div class="mb-1">
+      <div class="pt-2 space-y-2" v-for="link in links" :key="link.code">
+        <a
+          :href="link.link"
+          target="_blank"
+          class="flex items-center p-2 text-base text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700"
+        >
+          <UiIcon :name="link.code" classes="h-8 rounded-full w-8 overflow-hidden block" />
+          <span class="ml-3" sidebar-toggle-item="">{{ link.text }}</span>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const links = [
+  {
+    code: "email",
+    link: "mailto:ktr716@yandex.ru",
+    text: "ktr716@yandex.ru",
+  },
+]
+</script>
+
+<style scoped></style>
---
+++ app/components/Aside/Photo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="p-4 mb-4 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 sm:p-6 dark:bg-gray-800"
+  >
+    <div
+      class="items-center sm:flex xl:block 2xl:flex sm:space-x-4 xl:space-x-0 2xl:space-x-4"
+    >
+      <img
+        class="mb-4 rounded-lg w-28 h-28 sm:mb-0 xl:mb-4 2xl:mb-0"
+        src="https://flowbite-admin-dashboard.vercel.app/images/users/bonnie-green-2x.png"
+        alt="Jese picture"
+      />
+      <div>
+        <h3 class="mb-1 text-xl font-bold text-gray-900 dark:text-white">
+          {{ $t("myname") }}
+        </h3>
+        <div class="mb-4 text-sm text-gray-500 dark:text-gray-400">
+          <span
+            class="rounded-full w-2 h-2 align-middle bg-green-500 inline-block"
+          ></span>
+          {{ $t("mystatus") }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
