9cbcfab392db5cce84e730460ed1b1db9b06f77e
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2025-10-14 21:23:31 +0300

    dark mode

---
+++ app/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex overflow-hidden dark:bg-gray-700 min-h-screen">
+    <Aside />
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
