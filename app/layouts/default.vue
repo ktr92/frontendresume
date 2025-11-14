494bde6f753991d2cd02a537fb84cb9f7b36c803
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2025-11-14 23:22:03 +0300

    add projects accordion

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   
-  <div class="lg:flex overflow-hidden dark:bg-gray-700 min-h-screen pb-48">
+  <div class="lg:flex overflow-hidden dark:bg-gray-700 min-h-screen pb-28">
     <Aside />
     <slot />
   </div>
