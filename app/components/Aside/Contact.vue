2ddf300f66920d48a47c4cfa12d59ca15693546e
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2025-11-27 12:22:44 +0300

    add translation

--- app/components/Aside/Contact.vue
+++ app/components/Aside/Contact.vue
@@ -2,31 +2,28 @@
   <div class="border-none">
     <template v-if="links">
       <div>
-         <div class="pt-2 space-y-2" v-for="item in links" :key="item.code">
-        <a
-          :href="item.link"
-          target="_blank"
-          class="flex items-center text-base  mb-4 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700"
-        >
-          <UiIcon
-            :name="item.code"
-            classes="h-8 rounded-full w-8  block"
-          />
-          <span class="ml-3" sidebar-toggle-item="">{{ item.text }}</span>
-        </a>
+        <div class="pt-2 space-y-2" v-for="item in links" :key="item.code">
+          <a
+            :href="item.link"
+            target="_blank"
+            class="flex items-center text-base mb-4 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700"
+          >
+            <UiIcon :name="item.code" classes="h-8 rounded-full w-8  block" />
+            <span class="ml-3" sidebar-toggle-item="">{{ item.text }}</span>
+          </a>
+        </div>
       </div>
-      </div>
-     
-      <hr>
-      <div class="pt-2 space-y-2 linksblock" >
-        <NuxtLinkLocale :to="isprojects ? '/' : '/projects'"
-          class="flex items-center text-base  mb-4 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700"
+
+      <hr />
+      <div class="pt-2 space-y-2 linksblock">
+        <NuxtLinkLocale
+          :to="isprojects ? '/' : '/projects'"
+          class="flex items-center text-base mb-4 text-gray-900 transition duration-75 rounded-lg hover:bg-gray-100 group dark:text-gray-200 dark:hover:bg-gray-700"
         >
-           <UiIcon
-            name="projects"
-            classes="h-8 rounded-full w-8  block"
-          />
-          <span class="ml-3 text-green-500 font-bold" sidebar-toggle-item="">{{ isprojects ? $t("index") : $t("portfolio") }}</span>
+          <UiIcon name="projects" classes="h-8 rounded-full w-8  block" />
+          <span class="ml-3 text-green-500 font-bold" sidebar-toggle-item="">{{
+            isprojects ? $t("index") : $t("portfolio")
+          }}</span>
         </NuxtLinkLocale>
       </div>
     </template>
@@ -41,7 +38,7 @@
 <script setup lang="ts">
 const links = useMainStore().getContentBy("contact")
 const route = useRoute()
-const isprojects = computed(() => route.path === '/projects')
+const isprojects = computed(() => route.path === "/projects")
 </script>
 
 <style scoped></style>
